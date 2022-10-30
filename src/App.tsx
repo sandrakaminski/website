9d4401d4447bd0e2eec9e374934359fab0820425
Your Name <you@example.com>
2022-10-31 08:55:16 +1300

    type declaration demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import '@/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, setCount] = useState<number>(0)
 
   return (
     <div className="App">
