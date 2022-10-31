e293b88d8bcd3792c40858c5012702e3a36a700d
Your Name <you@example.com>
2022-11-01 11:07:56 +1300

    added theme type

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,10 +1,12 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
+import type { Theme } from '@mui/material/styles';
 // import { components } from './components';
 import { palette } from './palette';
 import { typography } from './typography';
 
-export const theme = responsiveFontSizes(createTheme({
+
+export const theme: Theme = responsiveFontSizes(createTheme({
     palette,
     typography,
     // components,
