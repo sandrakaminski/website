a0934a17b24d490d0761191a8e43853b25ff4d50
Your Name <you@example.com>
2022-11-01 11:32:06 +1300

    improved header func and ui

--- src/Viewport/Header.tsx
+++ src/Viewport/Header.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 
+import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Toolbar from "@mui/material/Toolbar";
 import Link from "@mui/material/Link";
 import { useNavigate } from "react-router-dom";
 
+import logo from '../assets/logo.png';
 import getKontentData from "../client";
 
 type Data = {
@@ -17,9 +22,18 @@ type Data = {
 
 const Header: React.FC = () => {
     const [data, getData] = useState<Data>({ nested_items: { value: [] } });
-    const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
 
+    const navigate = useNavigate();
     getKontentData({ name: "assembly", getData });
+
+    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(e.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const handleNavigate = (path: string) => {
         if (path === "home") {
@@ -34,18 +48,30 @@ const Header: React.FC = () => {
     const filter: string[] = data.nested_items?.value.filter((item: string) => item !== "home");
 
     return (
-        <AppBar elevation={0}>
+        <AppBar color="transparent" position="static" elevation={0}>
             <Toolbar>
                 <Box sx={{ flexGrow: 1 }}>
                     <Link onClick={() => handleNavigate('home')} component="button" sx={{ cursor: 'pointer' }} underline="none" color="inherit">
-                        Sandra Kaminski
+                        <img style={{ width: 200, height: 'auto' }} src={logo} alt="Sandra Kaminski" />
                     </Link>
                 </Box>
-                {filter?.map((item: string, index: number) =>
-                    <Button color="inherit" onClick={() => handleNavigate(item)} key={index}>
-                        {item}
-                    </Button>
-                )}
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    {filter?.map((item: string, index: number) =>
+                        <Button color="inherit" onClick={() => handleNavigate(item)} key={index}>
+                            {item}
+                        </Button>
+                    )}
+                </Box>
+                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton onClick={handleClick} color="inherit">
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu anchorEl={anchorEl} open={open} onClose={handleClose}   >
+                        {filter?.map((item: string, index: number) =>
+                            <MenuItem key={index} sx={{ textTransform: 'capitalize' }} onClick={() => { handleNavigate(item), handleClose() }}>{item}</MenuItem>
+                        )}
+                    </Menu>
+                </Box>
             </Toolbar>
         </AppBar>
 
--- src/Viewport/index.tsx
+++ src/Viewport/index.tsx
@@ -1,31 +1,23 @@
 import React from 'react'
 
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 import Header from './Header';
 import Footer from './Footer';
 
-interface LayoutProps {
-    children: React.ReactNode
+type ViewportProps = {
+    children: React.ReactNode;
 }
 
-const Viewport = (props: LayoutProps) => {
-    const { children } = props;
-
+const Viewport = ({ children }: ViewportProps) => {
     return (
-        <Box sx={{
-            mt: 10,
-            display: 'flex',
-            flexDirection: 'column'
-        }}
-        >
+        <>
             <Header />
             <Container sx={{ minHeight: '100vh' }} maxWidth="lg">
                 {children}
             </Container>
             <Footer />
-        </Box>
+        </>
     )
 }
 
