e6749d0900a3812b28668d14b0988bd47b360d41
finest08 <mr.mitcholl@gmail.com>
2022-11-01 10:58:28 +1300

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React, { Suspense } from 'react'
 
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
 
 import Viewport from './Viewport'
 import Routes from './Routes'
 
+import { theme } from './theme';
+
 export const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <Viewport>
-        <Suspense fallback={<p>...loading</p>}>
-          <Routes />
-        </Suspense>
-      </Viewport>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Viewport>
+          <Suspense fallback={<p>...loading</p>}>
+            <Routes />
+          </Suspense>
+        </Viewport>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
