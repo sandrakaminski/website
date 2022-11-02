dda478fecd7223403ea224de53cc05f948af4b42
finest08 <mr.mitcholl@gmail.com>
2022-11-02 13:10:19 +1300

    changes to hero components

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import CardMedia from '@mui/material/CardMedia';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -37,25 +38,26 @@ const Component: React.FC = () => {
     return (
         <>
             {content ?
-                <Box >
-                    <Stack direction="column" alignItems="center" justifyContent="center" sx={{ p: 8 }}>
-                        {content?.fields.references[0].fields.heroImage?.fields.file.url &&
-                            <CardMedia
-                                component="img"
-                                src={content.fields.references[0].fields.heroImage.fields.file.url}
-                                alt={content.fields.references[0].fields.heroImage.fields.title}
-                                width="auto"
-                                height="100%"
-                            />
-                        }
-                        <Typography variant="h1" component="h1" sx={{ p: 10 }}>
-                            {content?.fields.references[0].fields.headline}
-                        </Typography>
-                        <Typography variant="body1" component="p">
-                            {content?.fields.references[0].fields.subheader}
-                        </Typography>
-                    </Stack>
-                </Box>
+                <Stack direction="column" alignItems="center" justifyContent="center" sx={{ p: 8 }}>
+                    {content?.fields.references[0].fields.heroImage?.fields.file.url &&
+                        <CardMedia
+                            component="img"
+                            src={content.fields.references[0].fields.heroImage.fields.file.url}
+                            alt={content.fields.references[0].fields.heroImage.fields.title}
+                            width="auto"
+                            height="100%"
+                        />
+                    }
+                    <Typography variant="h1" component="h1" sx={{ p: 6 }}>
+                        {content?.fields.references[0].fields.headline}
+                    </Typography>
+                    <Container maxWidth="sm">
+                        <Typography variant="body2" component="p">
+                        {content?.fields.references[0].fields.subheader}
+                    </Typography>
+                    </Container>
+                    
+                </Stack>
                 :
                 <Outline />
             }
