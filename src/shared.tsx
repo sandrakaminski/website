c6413b8ba49ae0be6438f6603973dcf18e2a0375
Your Name <you@example.com>
2022-11-03 13:35:59 +1300

    shared components

--- src/shared.tsx
+++ src/shared.tsx
@@ -1,11 +1,4 @@
 import Button from '@mui/material/Button';
-import * as PropTypes from 'prop-types';
-
-export const BlockPropTypes = {
-    content: PropTypes.object.isRequired,
-    className: PropTypes.string,
-    detail: PropTypes.bool,
-};
 
 export const Markdown = {
     a: ({ ...props }: any) => <Button size="large" sx={{ my: 2 }} variant="outlined" {...props} />,
