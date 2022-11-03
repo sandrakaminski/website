c729245acbbd2e3212be1d9d845405b2a14d02f5
finest08 <mr.mitcholl@gmail.com>
2022-11-03 16:28:45 +1300

    organized section types

---
+++ src/blocks/Section/Left.tsx
@@ -0,0 +1,37 @@
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CardMedia from '@mui/material/CardMedia';
+import Stack from '@mui/material/Stack';
+
+import ReactMarkdown from 'react-markdown';
+import { Markdown } from '../../shared';
+
+type Content = {
+    content: any
+}
+
+export const Left = (props: Content) => {
+    const { content } = props;
+
+    return (
+        <Stack direction="column" alignItems="center" justifyContent="center" sx={{ p: 8 }}>
+            {content.fields.image?.fields.file.url &&
+                <CardMedia
+                    sx={{ width: content.fields.bannerStyle === false ? "60%" : "100%", height: 'auto' }}
+                    component="img"
+                    src={content.fields.image.fields.file.url}
+                    alt={content.fields.image.fields.title}
+                />
+            }
+            <Typography variant="h1" component="h1" sx={{ p: 6 }}>
+                {content.fields.headline}
+            </Typography>
+            <Container maxWidth="sm">
+                <ReactMarkdown components={Markdown} >
+                    {content.fields.body}
+                </ReactMarkdown>
+            </Container>
+        </Stack>
+    )
+}
+export default Left;
--- src/blocks/Section/index.tsx
+++ src/blocks/Section/index.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import Center from './Center';
+import Right from './Right';
+import Left from './Left';
 
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import CardMedia from '@mui/material/CardMedia';
-import Stack from '@mui/material/Stack';
-
-import ReactMarkdown from 'react-markdown';
-import { Markdown } from '../../shared';
-
+type Content = {
+    content: any
+}
 
 export const Section = (props: Content) => {
     const { content } = props;
@@ -25,81 +22,3 @@ export const Section = (props: Content) => {
 }
 export default Section;
 
-type Content = {
-    content: any
-}
-
-const Center = (props: Content) => {
-    const { content } = props;
-
-    return (
-        <Stack direction="column" alignItems="center" justifyContent="center" sx={{ p: 8 }}>
-            {content.fields.image?.fields.file.url &&
-                <CardMedia
-                    sx={{ width: content.fields.bannerStyle === false ? "60%" : "100%", height: 'auto' }}
-                    component="img"
-                    src={content.fields.image.fields.file.url}
-                    alt={content.fields.image.fields.title}
-                />
-            }
-            <Typography variant="h1" component="h1" sx={{ p: 6 }}>
-                {content.fields.headline}
-            </Typography>
-            <Container maxWidth="sm">
-                <ReactMarkdown components={Markdown} >
-                    {content.fields.body}
-                </ReactMarkdown>
-            </Container>
-        </Stack>
-    )
-}
-
-const Left = (props: Content) => {
-    const { content } = props;
-
-    return (
-        <Stack direction="column" alignItems="center" justifyContent="center" sx={{ p: 8 }}>
-            {content.fields.image?.fields.file.url &&
-                <CardMedia
-                    sx={{ width: content.fields.bannerStyle === false ? "60%" : "100%", height: 'auto' }}
-                    component="img"
-                    src={content.fields.image.fields.file.url}
-                    alt={content.fields.image.fields.title}
-                />
-            }
-            <Typography variant="h1" component="h1" sx={{ p: 6 }}>
-                {content.fields.headline}
-            </Typography>
-            <Container maxWidth="sm">
-                <ReactMarkdown components={Markdown} >
-                    {content.fields.body}
-                </ReactMarkdown>
-            </Container>
-        </Stack>
-    )
-}
-
-const Right = (props: Content) => {
-    const { content } = props;
-
-    return (
-        <Stack direction="column" alignItems="center" justifyContent="center" sx={{ p: 8 }}>
-            {content.fields.image?.fields.file.url &&
-                <CardMedia
-                    sx={{ width: content.fields.bannerStyle === false ? "60%" : "100%", height: 'auto' }}
-                    component="img"
-                    src={content.fields.image.fields.file.url}
-                    alt={content.fields.image.fields.title}
-                />
-            }
-            <Typography variant="h1" component="h1" sx={{ p: 6 }}>
-                {content.fields.headline}
-            </Typography>
-            <Container maxWidth="sm">
-                <ReactMarkdown components={Markdown} >
-                    {content.fields.body}
-                </ReactMarkdown>
-            </Container>
-        </Stack>
-    )
-}
