8ea734352e500767b7236d28f0c41ecf09973c9a
Your Name <you@example.com>
2022-11-04 08:27:15 +1300

    pofile item finsihed

--- src/blocks/About/Detail.tsx
+++ src/blocks/About/Detail.tsx
@@ -12,44 +12,60 @@ const Detail = (props: ContentProps) => {
     const { content } = props;
 
     return (
-        <Stack>
-            {content.fields.image &&
-                <CardMedia
-                    component="img"
-                    sx={{ width: '50%', height: 'auto' }}
-                    src={content?.fields.image.fields.file.url}
-                    alt={content.fields.image.fields.title}
-                />
-            }
-            <Typography variant="h2" component="caption" sx={{ p: 6 }}>
-                {content.fields.name}
-            </Typography>
-            <Typography variant="h2" component="h2" sx={{ p: 6 }}>
+        <Stack sx={{ my: 4 }} spacing={2} justifyContent="center" alignItems="center">
+            <Stack sx={{ mt: 4 }} spacing={1} alignItems="flex-start">
+                {content.fields.image &&
+                    <CardMedia
+                        component="img"
+                        sx={{ width: '100%', height: 'auto' }}
+                        src={content?.fields.image.fields.file.url}
+                        alt={content.fields.image.fields.title}
+                    />
+                }
+                <Typography variant="caption" >
+                    {content.fields.name}
+                </Typography>
+            </Stack>
+            <Typography variant="h2" component="h2" >
                 {content.fields.title}
             </Typography>
             <Container maxWidth="sm">
                 <ReactMarkdown components={Markdown} >{content.fields.body}</ReactMarkdown>
+                <Stack sx={{ mt: 2 }} direction="row" spacing={2} alignItems="flex-start">
+                    {content.fields.otherImages.map((image: any, index: number) =>
+                        <CardMedia
+                            key={index}
+                            component="img"
+                            sx={{ width: '50%', height: 'auto' }}
+                            src={image.fields.file.url}
+                            alt={image.fields.file.title}
+                        />
+                    )}
+                </Stack>
             </Container>
         </Stack>
     )
 }
 export default Detail;
 
-export type ContentProps = {
+type ContentProps = {
     content: {
         fields: {
             title: string;
             name: string;
             headline: string;
             body: string;
-            image: {
-                fields: {
-                    file: {
-                        url: string;
-                    }
-                    title: string;
-                }
-            }
+            otherImages: Image[];
+            image: Image
         }
+    }
+}
+
+type Image = {
+    fields: {
+        file: {
+            url: string;
+        }
+        title: string;
     }
 }
--- src/shared.tsx
+++ src/shared.tsx
@@ -1,7 +1,9 @@
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export const Markdown = {
-    a: ({ ...props }: any) => <Button size="large" sx={{ my: 2 }} variant="outlined" {...props} />,
-    img: ({ ...props }: any) => <img style={{ width: '60vw' }} {...props} />
+    a: ({ ...props }: any) => <Button target="_blank" rel="noopener noreferrer" size="large" sx={{ my: 2 }} variant="outlined" {...props} />,
+    img: ({ ...props }: any) => <img style={{ width: '60vw' }} {...props} />,
+    p: ({ ...props }: any) => <Typography color="grayText" variant="body1" {...props} />,
 };
 
