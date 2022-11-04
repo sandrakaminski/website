e619596074ba86779636363a29f427f551618f81
finest08 <mr.mitcholl@gmail.com>
2022-11-04 14:47:40 +1300

    left section component

--- src/blocks/Section/Left.tsx
+++ src/blocks/Section/Left.tsx
@@ -1,7 +1,8 @@
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import CardMedia from '@mui/material/CardMedia';
-import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import { Link } from "react-router-dom";
 
 import ReactMarkdown from 'react-markdown';
 import { Markdown } from '../../shared';
@@ -13,25 +14,67 @@ type Content = {
 export const Left = (props: Content) => {
     const { content } = props;
 
+    const title = content.fields.resources[0].fields.files;
+    const title2 = content.fields.resources[1].fields.files; //[0].fields.title
+
     return (
-        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ p: 10 }}>
-            {content.fields.image?.fields.file.url &&
-                <CardMedia
-                    sx={{ width: content.fields.bannerStyle === false ? "60%" : "50%", height: 'auto' }}
-                    component="img"
-                    src={content.fields.image.fields.file.url}
-                    alt={content.fields.image.fields.title}
-                />
-            }
-            <Typography align="center" variant="h1" component="h1" sx={{ p: 6 }}>
-                {content.fields.image.fields.title}
-            </Typography>
+        <Grid container spacing={2} sx={{ p: 10 }}>
+            <Grid item lg={6}>
+                {content.fields.image?.fields.file.url &&
+                    <CardMedia
+                        sx={{ width: content.fields.bannerStyle === false ? "60%" : "100%", height: 'auto' }}
+                        component="img"
+                        src={content.fields.image.fields.file.url}
+                        alt={content.fields.image.fields.title}
+                    />
+                }
+            </Grid>
+            <Grid item lg={6}>
+                <Typography align="center" variant="h1" component="h1" sx={{ p: 4 }}>
+                    {content.fields.resources[0].fields.headline}
+                </Typography>
+                {title.map((item: any, index: number) => (
+                    <Typography key={index} align="center" variant="body1" component="h1">
+                        <Link to={`${item.fields.file.url}`} target="_blank">
+                            {item.fields.title}
+                        </Link>
+                    </Typography>
+                    
+                ))}
+                <Typography align="center" variant="h4" component="h1" sx={{ p: 4 }}>
+                    {content.fields.resources[1].fields.headline}
+                </Typography>
+                {title2.map((item: any, index: number) => (
+                    <Typography key={index} align="center" variant="body1" component="h1">
+                        <Link to={`${item.fields.file.url}`} target="_blank">
+                            {item.fields.title}
+                        </Link>
+                    </Typography>
+
+                ))}
+                <Grid item lg={6}>
+                    
+
+                </Grid>
+
+            </Grid>
             <Container maxWidth="sm">
                 <ReactMarkdown components={Markdown} >
                     {content.fields.body}
                 </ReactMarkdown>
             </Container>
-        </Stack>
+        </Grid >
+
     )
 }
 export default Left;
+
+
+
+{/* title.set(key, content.fields.resources[0].fields.files[0].fields.title[key])} */ }
+
+
+{/* <Link to={`${content.fields.resources[0].fields.files[0].fields.file.url}`}>
+                         
+                            {content.fields.resources[0].fields.files[0].fields.title}
+                        </Link> */}
