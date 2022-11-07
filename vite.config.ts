c337e11b65768b7a7cae31e126b751373c706342
Your Name <you@example.com>
2022-11-08 08:54:28 +1300

    made port a single instance

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-// import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+type Port = {
+  host: boolean;
+  port: number;
+}
 
+const port: Port = {
+  host: true,
+  port: 3000
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000
-  },
-  preview: {
-    host: true,
-    port: 3000
-  },
+  server: port,
+  preview: port,
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
