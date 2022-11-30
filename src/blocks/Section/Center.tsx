ffa7b52f19a94f85801973cc42532832c252af1f
Your Name <you@example.com>
2022-12-01 09:38:47 +1300

    added margin

--- src/blocks/Section/Center.tsx
+++ src/blocks/Section/Center.tsx
@@ -23,7 +23,7 @@ export const Center = (props: Content) => {
                 />
             }
             {content.fields.headline &&
-                <Typography align="center" variant="h2" sx={{ pt: { xs: 2, md: 4 } }}>
+                <Typography align="center" variant="h2" sx={{ pt: { xs: 2, md: 4 }, my: 2 }}>
                     {content.fields.headline}
                 </Typography>
             }
@@ -37,7 +37,7 @@ export const Center = (props: Content) => {
                     </Box>
                 </Stack>
             ))}
-            <Stack justifyContent="center" direction="column" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText'}}>
+            <Stack justifyContent="center" direction="column" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText' }}>
                 <ReactMarkdown components={SectionMarkDown} >
                     {content.fields.body}
                 </ReactMarkdown>
