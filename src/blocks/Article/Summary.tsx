38a7512f58634f176f45052504c18b7dd227f4db
finest08 <mr.mitcholl@gmail.com>
2022-12-06 14:44:11 +1300

    article expand

--- src/blocks/Article/Summary.tsx
+++ src/blocks/Article/Summary.tsx
@@ -2,58 +2,45 @@ import { useState } from 'react';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from '@mui/material/CardContent';
 import CardHeader from "@mui/material/CardHeader";
-import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
-import { styled } from '@mui/material/styles';
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 
 import type { ArticleType } from "./ArticleType";
 import DateFormatter from "@/components/DateFormatter";
 import LoadingImage from '@/components/LoadingImage';
 
-interface ExpandMoreProps {
-    expand: string;
-    onClick: () => void;
-    children: React.ReactNode;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-    const { ...other } = props;
-    return <IconButton {...other} sx={{ display: 'flex', mx: 1 }} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}))
-
 const Summary = (props: ArticleType) => {
     const [expanded, setExpanded] = useState(false);
+    const [hidden] = useState<boolean>(false);
     const { content } = props;
     const { pathname } = useLocation();
     const { slug } = useParams();
     const navigate = useNavigate();
-
-    const expandCard = () => {
-        setExpanded(!expanded);
-    }
+    const preview = content.fields.headline.length > 25 ? `${content.fields.headline.substring(0, 25)}...` : content.fields.headline;
+    const [showMore, setShowMore] = useState<string>(preview);
 
     const cardRoot = {
-        maxHeight: 45,
         display: 'block',
-        overflow: 'hidden',
         textAlign: 'center',
     }
 
     const cardRootExpand = {
         display: 'block',
+    }
+
+    const handleShowMore = () => {
+        setExpanded(!expanded);
+    }
+
+    const handleShowLess = () => {
+        setShowMore(preview)
     }
 
     return (
@@ -65,24 +52,26 @@ const Summary = (props: ArticleType) => {
                     alt={content.fields.coverImage.fields.title}
                 />
             </CardActionArea>
-            <CardHeader style={expanded ? cardRootExpand : cardRoot} title={content.fields.headline} />
-            <ExpandMore
-                expand={expanded ? 'true' : ''}
-                onClick={expandCard}
-                aria-expanded={expanded}
-                aria-label="show more"
-            >
-                <ExpandMoreIcon />
-            </ExpandMore>
-            <Collapse in={expanded} unmountOnExit>
-            </Collapse>
+            <CardHeader style={expanded ? cardRootExpand : cardRoot} title={expanded ? content.fields.headline : preview} />
+            {content.fields.headline.length > 25 &&
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end', mx: 1 }}>
+                    {!hidden && showMore === preview ?
+                        <IconButton onClick={handleShowMore} sx={{ transform: !expanded ? 'rotate(0deg)' : 'rotate(180deg)' }}>
+                            <ExpandMoreIcon />
+                        </IconButton>
+                        :
+                        <IconButton onClick={handleShowLess} >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    }
+                </Box>
+            }
             <CardContent component={Stack} direction="row" justifyContent="space-between" alignItems="center" spacing={0.5}>
                 <DateFormatter dateString={content.fields.date} />
                 <FiberManualRecordIcon sx={{ width: 5, height: 5 }} />
                 <Link underline="hover" sx={{ cursor: 'pointer' }} onClick={() => navigate(`/about/${content.fields.author.fields.slug}`)} variant="body2">
                     {content.fields.author.fields.name}
                 </Link>
-
             </CardContent>
         </Card>
     )
