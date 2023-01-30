c848592187792d51641db6601a867f4f2e82ecad
Your Name <you@example.com>
2023-01-30 15:06:42 +1300

    changed list text colour

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -13,9 +13,10 @@ type Img = {
 export const Markdown: Record<string, unknown> = {
     a: ({ ...props }) => <Link target="_blank"  {...props} />,
     img: ({ ...props }, { alt, src }: Img) => <img style={{ width: '100%' }} src={src} alt={alt} {...props} />,
-    p: ({ ...props }) => <Typography sx={{ lineHeight: 1.75 }} paragraph align="justify" variant="body1" {...props} />,
+    p: ({ ...props }) => <Typography sx={{ lineHeight: 1.75 }} color="grayText" paragraph align="justify" variant="body1" {...props} />,
     hr: ({ ...props }) => <Divider sx={{ my: 2 }} {...props} />,
     div: ({ ...props }) => <Stack justifyContent="center" alignItems="center" direction="row" {...props} />,
+    ul: ({ ...props }) => <Typography sx={{ lineHeight: 1.75 }} color="grayText" paragraph align="justify" variant="body1" {...props} />,
 }
 
 export const SectionMarkDown = {
