d7739fb62f854943fdd86d1545b09286111b0a04
Your Name <you@example.com>
2023-01-31 16:04:41 +1300

    ADDED COMMENTS

--- functions/comments/go.mod
+++ functions/comments/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/aws/aws-lambda-go v1.37.0
+	github.com/joho/godotenv v1.4.0
 	go.mongodb.org/mongo-driver v1.11.1
 )
 
--- functions/comments/main.go
+++ functions/comments/main.go
@@ -4,26 +4,27 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type comments struct {
+type Comments struct {
 	Name    string `json:"name"`
 	Comment string `json:"comment"`
 	ID      string `json:"id"`
 	Date    int64  `json:"date"`
 }
 type Page struct {
-	Data    []comments `json:"data"`
+	Data    []Comments `json:"data"`
 	Matches int64      `json:"matches"`
 }
 type Store struct {
@@ -48,7 +49,9 @@ func Connect() Store {
 	}
 }
 
-func (s *Store) AddComment(c comments) {
+// stores
+
+func (s *Store) AddComment(c Comments) {
 	insertResult, err := s.locaColl.InsertOne(context.Background(), c)
 	if err != nil {
 		log.Fatal(err)
@@ -56,19 +59,45 @@ func (s *Store) AddComment(c comments) {
 	fmt.Printf("\nInserted a Single Document: %v\n", insertResult)
 }
 
-func (c *CommentStore) handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	reqByt, err := io.ReadAll(events.APIGatewayProxyRequest.Body)
-	if err != nil {
-		return nil, err
-	}
-	var coms comments
-	json.Unmarshal(reqByt, &coms)
-	coms.Date = time.Now().Unix()
-	c.Store.AddComment(coms)
+func (s *Store) GetComments(searchText string, limit, skip *int64) (Page, error) {
+	filter := bson.M{}
 
-	if err != nil {
-		return nil, fmt.Errorf("error generating payment link: %w", err)
+	if searchText != "" {
+		filter = bson.M{"$text": bson.M{"$search": `"` + searchText + `"`}}
 	}
+	opt := options.FindOptions{
+		Skip:  skip,
+		Limit: limit,
+		Sort:  bson.M{"date": -1},
+	}
+
+	mctx := context.Background()
+	cursor, err := s.locaColl.Find(mctx, filter, &opt)
+	if err != nil {
+		return Page{}, err
+	}
+
+	// unpack results
+	var pg Page
+	if err := cursor.All(mctx, &pg.Data); err != nil {
+		return Page{}, err
+	}
+	if pg.Matches, err = s.locaColl.CountDocuments(mctx, filter); err != nil {
+		return Page{}, err
+	}
+	return pg, nil
+}
+
+// handlers
+
+func (s *Store) Create(r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var coms Comments
+	if err := json.Unmarshal([]byte(r.Body), &coms); err != nil {
+		log.Fatal(err)
+	}
+	coms.Date = time.Now().Unix()
+	s.AddComment(coms)
+
 	rspByt, err := json.Marshal(coms)
 	if err != nil {
 		return nil, fmt.Errorf("error marshaling payment link: %w", err)
@@ -79,12 +108,47 @@ func (c *CommentStore) handler(request events.APIGatewayProxyRequest) (*events.A
 		Headers:    map[string]string{"Content-Type": "application/json"},
 		Body:       string(rspByt),
 	}, nil
+}
 
+func (s *Store) Get(r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	limit, err := strconv.ParseInt(r.QueryStringParameters["limit"], 10, 64)
+	if err != nil {
+		limit = 10
+	}
+	skip, err := strconv.ParseInt(r.QueryStringParameters["skip"], 10, 64)
+	if err != nil {
+		skip = 0
+	}
+	searchText := r.QueryStringParameters["searchText"]
+	pg, err := s.GetComments(searchText, &limit, &skip)
+	if err != nil {
+		return nil, fmt.Errorf("error getting comments: %w", err)
+	}
+
+	rspByt, err := json.Marshal(pg)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling payment link: %w", err)
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(rspByt),
+	}, nil
+}
+
+func handler(r events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	cmmntSt := CommentStore{Store: Connect()}
+	switch r.HTTPMethod {
+	case "POST":
+		return cmmntSt.Store.Create(r)
+	case "GET":
+		return cmmntSt.Store.Get(r)
+	default:
+		return nil, fmt.Errorf("unsupported method: %s", r.HTTPMethod)
+	}
 }
 
 func main() {
-	c := CommentStore{
-		Store: Connect(),
-	}
-	lambda.Start(c)
+	lambda.Start(handler)
 }
