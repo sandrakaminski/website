32a7ceca5e55d76d0c1d67096153232e4557c989
Your Name <you@example.com>
2023-02-22 17:01:07 +1300

    removed imports

--- src/views/Content/Products/products.test.tsx
+++ src/views/Content/Products/products.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { Entry } from "contentful";
 import { BrowserRouter as Router } from "react-router-dom";
 import { describe, test, expect } from 'vitest'
