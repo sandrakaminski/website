817c61d0f88f4045c37f0021ddb63ed73b4a35e7
Zachary Weston <zachary.weston@propellerhead.co.nz>
2024-02-06 19:07:03 +1300

    added jsx to component

--- src/components/ToggleStory.tsx
+++ src/components/ToggleStory.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { JSX } from "react";
 
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
