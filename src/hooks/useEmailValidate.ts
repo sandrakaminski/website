83eb9818aa575676bc852c5162fa513452184a0c
Zachary Weston <zachary.weston@propellerhead.co.nz>
2024-03-07 16:09:41 +1300

    fixed email fields

--- src/hooks/useEmailValidate.ts
+++ src/hooks/useEmailValidate.ts
@@ -1,12 +1,14 @@
 type ValidEmail = {
     email: string;
+    firstName: string;
+    lastName: string;
     errors: {
         [x: string]: string;
     };
 }
 
 export const useEmailValidate = (state: ValidEmail): boolean => {
-    if (!state.email) {
+    if (!state.email || !state.firstName || !state.lastName) {
         return false;
     }
     for (const err in state.errors) {
